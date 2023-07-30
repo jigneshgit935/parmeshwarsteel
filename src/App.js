50219bf459752964138138ab33180cdeedd5da74
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-31 00:31:53 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Home from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
 import Product from './pages/Product/Product';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <ScrollToTop />
         <Route path="/" element={<Home />} />
         <Route path="products" element={<Product />} />
         <Route path="contact" element={<Contact />} />
