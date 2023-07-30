91ae5a42d5ffd70296831cd5598dabf013bac434
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-30 23:58:57 +0530

    added

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-
+import { Link } from 'react-router-dom';
+import { AiOutlineWhatsApp } from 'react-icons/ai';
+import './Layout.css';
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar />
-      {children}
-      <Footer />
+      <div>
+        <Navbar />
+        {children}
+
+        <Footer />
+        <Link
+          to="https://wa.me/918693846478"
+          target="_blank"
+          className="whatsapp_float d-flex justify-content-center align-items-center"
+        >
+          <AiOutlineWhatsApp className="" size={50} />
+        </Link>
+      </div>
     </>
   );
 };
