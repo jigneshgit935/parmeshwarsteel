940b9fa8baec0b23505369535c8e4c73010936db
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-07 04:35:34 +0530

    upadting

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
 import Product from './pages/Product/Product';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import About from './components/About/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/" element={<Product />} />
+        <Route path="/about/" element={<About />} />
         <Route path="/contact/" element={<Contact />} />
       </Routes>
     </div>
