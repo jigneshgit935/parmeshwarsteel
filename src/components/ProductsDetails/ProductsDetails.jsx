a98bb82aa559104dfd63373693fdd8e0b5aeacfb
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-07 05:26:37 +0530

    back to product page updated

--- src/components/ProductsDetails/ProductsDetails.jsx
+++ src/components/ProductsDetails/ProductsDetails.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Layout from '../Layout/Layout';
+import { Link } from 'react-router-dom';
 
 const ProductsDetails = (props) => {
   return (
     <Layout>
-      <div className="py-5 px-5 bg-danger">
-        <h1 className="display-2">{props.h1}</h1>
+      <div>
+        <Link to="/products">
+          <button className="px-3 py-2 rounded bg-primary">
+            Back to Products Page
+          </button>
+        </Link>
+        <div className="py-5 px-5 bg-danger">
+          <h1 className="display-2">{props.h1}</h1>
+        </div>
       </div>
     </Layout>
   );
