5e98302927881b9a33de3d50d771896832a0b10d
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-07 05:21:29 +0530

    updates

--- src/components/ProductsDetails/ProductsDetails.jsx
+++ src/components/ProductsDetails/ProductsDetails.jsx
@@ -4,7 +4,7 @@ import Layout from '../Layout/Layout';
 const ProductsDetails = (props) => {
   return (
     <Layout>
-      <div className="py-5 bg-danger">
+      <div className="py-5 px-5 bg-danger">
         <h1 className="display-2">{props.h1}</h1>
       </div>
     </Layout>
