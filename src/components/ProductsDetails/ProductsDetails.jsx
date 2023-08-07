c10f64ec31c3082602361ae672622c56fd3881db
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-07 05:36:01 +0530

    updated

--- src/components/ProductsDetails/ProductsDetails.jsx
+++ src/components/ProductsDetails/ProductsDetails.jsx
@@ -5,13 +5,11 @@ import { Link } from 'react-router-dom';
 const ProductsDetails = (props) => {
   return (
     <Layout>
-      <div className="bg-primary py-5">
+      <div className="bg-primary py-5 px-5">
         <Link to="/products">
-          <button className="px-3 py-2 my-3 rounded ">
-            Back to Products Page
-          </button>
+          <button className="px-3 py-2 rounded ">Back to Products Page</button>
         </Link>
-        <div className="py-5 px-5 bg-danger">
+        <div className="my-5 bg-danger">
           <h1 className="display-2">{props.h1}</h1>
         </div>
       </div>
