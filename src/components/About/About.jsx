43118a1520a1908a4d8f509eff66881264020d2d
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-12 07:27:56 +0530

    updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -27,14 +27,20 @@ const About = () => {
                 <h1 className="fw-bold underline-for-heading">About Us</h1>
               </div>
               <div className="">
-                <p className="mt-lg-4 mt-3" style={{ fontSize: '18px' }}>
+                <p
+                  className="mt-lg-4 mt-3"
+                  style={{ fontSize: '18px', textAlign: 'justify' }}
+                >
                   We, Parmeshwar Steel Center founded in 1980's by our visionary
                   entrepreneur Shri XYZ having an experience of more than 3
                   decades in this field, Parmeshwar Steel Center is now a well
                   established and learning manufacturers and suppliers of&nbsp;
                   <b>PIPE FITTINGS, FLANGES and FASTENERS</b>.
                 </p>
-                <p className="mt-lg-4 mt-3" style={{ fontSize: '18px' }}>
+                <p
+                  className="mt-lg-4 mt-3"
+                  style={{ fontSize: '18px', textAlign: 'justify' }}
+                >
                   In today's competitive and demanding market place it takes
                   more than just supply of material to become a reliable
                   supplier. We at PSC have painstakingly master the Supply Chain
@@ -43,7 +49,10 @@ const About = () => {
                   demand, scalabilty and efficient management to become a real "
                   <b>Partner in Progress</b>" for clients.
                 </p>
-                <p className="mt-lg-4 mt-3" style={{ fontSize: '18px' }}>
+                <p
+                  className="mt-lg-4 mt-3"
+                  style={{ fontSize: '18px', textAlign: 'justify' }}
+                >
                   Our satisfied and repeat customers of over 3 decades are the
                   who's and who of Oil & Gas, Petrochemicals, Refineries, Power,
                   Steel, Chemical & Fertilizer, Water Treatment, Offshore,
