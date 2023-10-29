6b10cbb245afb05caf3299da2076b09ab0b28087
Jignesh <jignesh.sharma.707@gmail.com>
2023-10-29 08:10:57 +0530

    uoad

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,66 +1,67 @@
 import React from 'react';
 import './Product.css';
 import Layout from '../../components/Layout/Layout';
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import TableContent from '../../components/TableContent/TableContent';
 import ProductCard from '../../components/ProductCard/ProductCard';
 
 const Product = () => {
-  // const textVariants = {
-  //   initial: {
-  //     x: -500,
-  //     opacity: 0,
-  //   },
-  //   animate: {
-  //     x: 0,
-  //     opacity: 1,
-  //     transition: {
-  //       duration: 1,
-  //       staggerChildren: 0.2,
-  //       type: 'spring',
-  //       stiffness: 100,
-  //       damping: 15,
-  //     },
-  //   },
-  // };
-  // const backgroundImage = {
-  //   initial: {
-  //     x: 0,
-  //     opacity: 0,
-  //   },
-  //   animate: {
-  //     x: 0,
-  //     opacity: 1,
-  //     transition: {
-  //       duration: 0.4,
-  //       staggerChildren: 0.1,
-  //     },
-  //   },
-  // };
+  const textVariants = {
+    initial: {
+      x: -500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        delay: 0.5,
+        staggerChildren: 0.2,
+        type: 'spring',
+        stiffness: 100,
+        damping: 14,
+      },
+    },
+  };
+  const backgroundImage = {
+    initial: {
+      x: 0,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.9,
+        staggerChildren: 0.1,
+      },
+    },
+  };
   return (
     <Layout>
-      <div className="pb-4">
-        <div
-          // variants={backgroundImage}
-          // initial="initial"
-          // animate="animate"
+      <motion.div className="pb-4">
+        <motion.div
+          variants={backgroundImage}
+          initial="initial"
+          animate="animate"
           className="text-center text-white imgBgProducts"
           style={{ padding: '80px 0px' }}
         >
-          <h1
-            // variants={textVariants}
-            // initial="initial"
-            // animate="animate"
+          <motion.h1
+            variants={textVariants}
+            initial="initial"
+            animate="animate"
             className="display-3 fw-bold"
           >
             Our Products
-          </h1>
-        </div>
+          </motion.h1>
+        </motion.div>
         <div className="mt-5">
           <ProductCard />
           <TableContent />
         </div>
-      </div>
+      </motion.div>
     </Layout>
   );
 };
