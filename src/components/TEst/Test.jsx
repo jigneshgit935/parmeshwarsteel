a6f65cd71b9d8a1ff255dc21b7779351990754e3
Jignesh <jignesh.sharma.707@gmail.com>
2023-10-29 12:09:57 +0530

    updatre

--- src/components/TEst/Test.jsx
+++ src/components/TEst/Test.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './Test.css';
 const Test = () => {
-  return <div className="test"></div>;
+  const backgroundImage = {
+    initial: {
+      x: 0,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.9,
+        staggerChildren: 0.1,
+      },
+    },
+  };
+  return (
+    <motion.div
+      variants={backgroundImage}
+      initial="initial"
+      animate="animate"
+      className="test"
+    ></motion.div>
+  );
 };
 
 export default Test;
